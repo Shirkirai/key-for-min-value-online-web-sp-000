e0c7bf75e626fc774c244a06a80f162e977b7ee8
Jung Lee <lee.jung.s95@gmail.com>
2020-04-29 02:17:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
+  if hash == {}
+    return nil
   hash.each do |key, value|
-    if hash == []
-      return nil
+    
     end
   end
 end
