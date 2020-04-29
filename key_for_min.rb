9b5c94dd42afd5373c3c4facfcbf9a9b8a57160e
Jung Lee <lee.jung.s95@gmail.com>
2020-04-29 02:37:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,11 @@ def key_for_min_value(hash)
   if hash == {}
     return nil
   end
+  i = 0
   hash.collect do |key, value|
-    key
+    until value[i] < value[i+1]
+      key_for_min_value(hash)
+      i += 1
+      return key
   end
 end
